27a2a71583b5e27a9634d2831849c464477b8a92
chee86j <jeffreychee86@gmail.com>
2023-09-16 18:48:22 -0400

    ignore update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import About from "./components/About";
