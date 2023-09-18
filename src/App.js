6abfbebf511b315f52d2b54654f6ec73a31d8794
chee86j <jeffreychee86@gmail.com>
2023-09-18 18:40:19 -0400

    loader added and Aos implemention base

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import About from "./components/About";
@@ -9,37 +10,58 @@ import Footer from "./components/Footer";
 import useDarkModeToggler from "./hooks/useDarkMode";
 import sunIcon from "./assets/sun.png";
 import moonIcon from "./assets/moon.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./App.css";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isDarkMode, toggleDarkMode] = useDarkModeToggler();
+  const [loading, setLoading] = useState(true);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 4000,
+    });
+  }, []);
+
   return (
     <div className="App">
-      <div className="dark-mode-toggler">
-        <input
-          type="checkbox"
-          id="darkmode-toggle"
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-        />
-        <label htmlFor="darkmode-toggle">
-          <img src={sunIcon} alt="Sun Icon" className="sun-icon" />
-          <img src={moonIcon} alt="Moon Icon" className="moon-icon" />
-        </label>
-      </div>
-      <Navbar toggleMenu={toggleMenu} />
-      <Profile id="profile" />
-      <About id="about" />
-      <Experience id="experience" />
-      <Projects id="projects" />
-      <Contact id="contact" />
-      <Footer />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="dark-mode-toggler" data-aos="fade-up">
+            <input
+              type="checkbox"
+              id="darkmode-toggle"
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+            />
+            <label htmlFor="darkmode-toggle">
+              <img src={sunIcon} alt="Sun Icon" className="sun-icon" />
+              <img src={moonIcon} alt="Moon Icon" className="moon-icon" />
+            </label>
+          </div>
+          <Navbar toggleMenu={toggleMenu} data-aos="fade-up" />
+          <Profile id="profile" data-aos="fade-up" />
+          <About id="about" data-aos="fade-up" />
+          <Experience id="experience" data-aos="fade-up" />
+          <Projects id="projects" data-aos="fade-up" />
+          <Contact id="contact" data-aos="fade-up" />
+          <Footer data-aos="fade-up" />
+        </>
+      )}
     </div>
   );
 }
